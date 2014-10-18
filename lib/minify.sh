7d917786d86419d08f6d792fb4e9ce7611292ec0
Camden Narzt <admin@silversshadows.com>
2014-10-18 00:36:07 -0600

    Add basic zooming

--- lib/minify.sh
+++ lib/minify.sh
@@ -5,15 +5,8 @@ cat public/javascripts/jquery.min.js \
     public/javascripts/jquery.flot.time.min.js \
     public/javascripts/jquery.flot.resize.min.js \
     public/javascripts/jquery.flot.selection.min.js \
+    public/javascripts/jquery.flot.navigate.min.js \
     public/javascripts/plugins.js \
     public/javascripts/main.js \
     public/javascripts/data.js \
-    > public/javascripts/application.tmp.js
-
-# java -jar ~/Downloads/yuicompressor-2.4.7.jar \
-#     -o public/javascripts/application.js \
-#     public/javascripts/application.tmp.js
-
-cp public/javascripts/application.tmp.js public/javascripts/application.js
-
-rm public/javascripts/application.tmp.js
+    > public/javascripts/application.js
