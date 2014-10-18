28a8b0e0f7cdbbf498edfe8a909849a104497feb
Camden Narzt <admin@silversshadows.com>
2014-10-17 23:26:13 -0600

    Updated Flot jQuery Library

--- lib/minify.sh
+++ lib/minify.sh
@@ -2,6 +2,7 @@
 
 cat public/javascripts/jquery.min.js \
     public/javascripts/jquery.flot.min.js \
+    public/javascripts/jquery.flot.time.min.js \
     public/javascripts/jquery.flot.resize.min.js \
     public/javascripts/jquery.flot.selection.min.js \
     public/javascripts/plugins.js \
