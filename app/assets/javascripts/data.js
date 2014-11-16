12c1e6ec4e6ce45802a28f439003a796884261ee
Camden Narzt <admin@silversshadows.com>
2014-11-16 15:28:10 -0700

    add ajax retry

--- app/assets/javascripts/data.js
+++ app/assets/javascripts/data.js
@@ -1,11 +1,24 @@
 var data = {}
 $.ajax({
   url:'/data',
+  tryCount : 0,
+  retryLimit : 3,
   success:function(r){
     $.extend(window.data,r);
     main();
   },
-  error:function(r){
-    console.log(r);
+  error:function(xhr, textStatus, errorThrown ) {
+    if (textStatus == 'timeout') {
+        this.tryCount++;
+        if (this.tryCount <= this.retryLimit) {
+            //try again
+            $.ajax(this);
+            return;
+        }
+        //give up
+        return;
+    }
+    //other errors
+    console.log(textStatus);
   }
 })
