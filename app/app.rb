7d9870bf6cf7633e571e2fa2b01d052b9fe6afd0
Camden Narzt <admin@silversshadows.com>
2014-12-04 01:11:35 -0700

    add flamegraph and other perf tools

--- app/app.rb
+++ app/app.rb
@@ -1,4 +1,5 @@
 require_relative "../lib/domainredirect.rb"
+require 'rack-mini-profiler'
 
 class Arewesmallyet < Padrino::Application
   register Padrino::Rendering
@@ -6,6 +7,7 @@ class Arewesmallyet < Padrino::Application
   register Padrino::Helpers
   register Padrino::Cache
 
+  use Rack::MiniProfiler
   use Rack::DomainRedirect, ["arewesmallyet.dev", "localhost"]
 
   disable :caching
