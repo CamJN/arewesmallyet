badf787934c378c9915c62100198b5d653567dba
Camden Narzt <admin@silversshadows.com>
2015-04-27 08:50:24 -0600

    allowing more automation

--- Rakefile
+++ Rakefile
@@ -1,4 +1,5 @@
 require 'padrino-core/cli/rake'
+require 'padrino-pipeline/tasks'
 PadrinoTasks.use(:sequel)
 PadrinoTasks.use(:database)
 PadrinoTasks.init
--- config/database.rb
+++ config/database.rb
@@ -2,7 +2,7 @@ Sequel::Model.plugin(:schema)
 #Sequel::Model.raise_on_save_failure = false # Do not throw exceptions on failure
 Sequel::Model.db = case Padrino.env
   when :development then Sequel.connect(adapter:'postgres', host:'localhost', port:5432, database:'arewesmallyet', user:'camdennarzt')
-  when :production  then Sequel.connect(ENV['DATABASE_URL'],  :loggers => [logger])
+  when :production  then Sequel.connect(ENV['DATABASE_URL'], loggers: [logger])
   when :test        then Sequel.connect(adapter:'postgres', host:'localhost', port:5432, database:'arewesmallyet_test', user:'camdennarzt')
 end
 
