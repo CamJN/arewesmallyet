2d08198d4a924fe56a72c7cb3b59df6e991f6e1b
Camden Narzt <admin@silversshadows.com>
2015-05-11 12:27:24 -0600

    fix stats js

--- app/assets/javascripts/data.js
+++ app/assets/javascripts/data.js
@@ -1,4 +1,27 @@
 var data = {}
+var stats = {}
+
+function goStats(){
+  $.ajax({
+    url:'stats.json',
+    tryCount : 0,
+    retryLimit : 3,
+    success:function(r){
+      $.extend(window.stats,r);
+      $(statistics);
+    },
+    error:function(xhr, textStatus, errorThrown ) {
+      this.tryCount++;
+      if (this.tryCount <= this.retryLimit) {
+        //try again
+        $.ajax(this);
+        return;
+      }
+      console.log(textStatus);
+    }
+  })
+}
+
 $.ajax({
   url:'data.json',
   tryCount : 0,
@@ -6,25 +29,7 @@ $.ajax({
   success:function(r){
     $.extend(window.data,r);
     $(main);
-  },
-  error:function(xhr, textStatus, errorThrown ) {
-    this.tryCount++;
-    if (this.tryCount <= this.retryLimit) {
-      //try again
-      $.ajax(this);
-      return;
-    }
-    console.log(textStatus);
-  }
-})
-var stats = {}
-$.ajax({
-  url:'stats.json',
-  tryCount : 0,
-  retryLimit : 3,
-  success:function(r){
-    $.extend(window.stats,r);
-    $(statistics);
+    goStats();
   },
   error:function(xhr, textStatus, errorThrown ) {
     this.tryCount++;
