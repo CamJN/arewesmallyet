3bbd2f97a4db61a6a1238a8b396210b3218822ed
Camden Narzt <admin@silversshadows.com>
2015-05-23 08:01:38 -0600

    cleanup production code

--- app/app.rb
+++ app/app.rb
@@ -6,12 +6,8 @@ class Arewesmallyet < Padrino::Application
   register Padrino::Helpers
   register Padrino::Cache
 
-  if (RACK_ENV != 'production')
-    use Rack::DomainRedirect, ["arewesmallyet.dev", "localhost"]
-  else
-    use Rack::DomainRedirect, ["arewesmallyet.com"]
-  end
-  
+  use Rack::DomainRedirect, ["arewesmallyet.dev", "localhost"] unless (RACK_ENV == 'production')
+
   disable :sessions
   disable :flash
   disable :asset_stamp
@@ -23,13 +19,14 @@ class Arewesmallyet < Padrino::Application
     config.prefix = '/public'
     config.enable_compression = true if (RACK_ENV == 'production')
   end
-  settings.assets.js_compressor = Uglifier.new(output: {comments: :none}, mangle: true) if (RACK_ENV == 'production')
 
   configure :production do
     enable :asset_stamp
     enable :caching
     expires 3600 * 12
     set :haml, ugly: true
+    settings.assets.js_compressor = Uglifier.new(output: {comments: :none}, mangle: true)
+    use Rack::DomainRedirect, ["arewesmallyet.com"]
     # You can customize caching store engines:
     #
     #   set :cache, Padrino::Cache::Store::Memcache.new(::Memcached.new('127.0.0.1:11211', exception_retry_limit: 1))
