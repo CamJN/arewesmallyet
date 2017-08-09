7d5d4c65c65b259b4cfbbfd32bb894c268b7223d
Camden Narzt <c.narzt@me.com>
2017-08-09 11:07:45 -0600

    Remove deprecated schema plugin
    it doesn’t seem to be used anyway

--- config/database.rb
+++ config/database.rb
@@ -1,5 +1,3 @@
-Sequel::Model.plugin(:schema)
-#Sequel::Model.raise_on_save_failure = false # Do not throw exceptions on failure
 Sequel::Model.db = case Padrino.env
   when :development then Sequel.connect(adapter:'postgres', host:'localhost', port:5432, database:'arewesmallyet', user:'camdennarzt')
   when :production  then Sequel.connect(ENV['DATABASE_URL'], loggers: [logger])
